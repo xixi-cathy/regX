bf469a315f485b8768836eb87dec98bbbd695604
xixi-cathy <57483791+xixi-cathy@users.noreply.github.com>
2024-06-12 14:17:38 +0800

    Add files via upload

---
+++ TensorDataset.py
@@ -0,0 +1,23 @@
+import torch
+from torch import Tensor
+from torch.utils.data import TensorDataset, DataLoader
+
+class TensorDataset(Dataset[Tuple[Tensor, ...]]):
+    r"""Dataset wrapping tensors.
+
+    Each sample will be retrieved by indexing tensors along the first dimension.
+
+    Args:
+        *tensors (Tensor): tensors that have the same size of the first dimension.
+    """
+    tensors: Tuple[Tensor, ...]
+
+    def __init__(self, *tensors: Tensor) -> None:
+        assert all(tensors[0].size(0) == tensor.size(0) for tensor in tensors), "Size mismatch between tensors"
+        self.tensors = tensors
+
+    def __getitem__(self, index):
+        return tuple(tensor[index] for tensor in self.tensors)
+
+    def __len__(self):
+        return self.tensors[0].size(0)
